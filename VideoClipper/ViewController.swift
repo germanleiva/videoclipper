ec1194bfbfa94576a2ced12f733e59c1990d98d4
psycr0w <leivagerman@gmail.com>
2015-07-01 18:24:10 +0200

    Initial Commit

---
+++ VideoClipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoClipper
+//
+//  Created by German Leiva on 01/07/15.
+//  Copyright © 2015 Germán Leiva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
